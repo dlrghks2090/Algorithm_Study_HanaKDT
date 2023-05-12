1173ce4b379da54f820e94ca97a359692fd8a715
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-05-12 10:32:49 +0900

    Add : 섬의 개수
    백준_실버2_BFS_4963번_섬의개수 문제 풀이

---
+++ 김익환/05.09.cpp
@@ -0,0 +1,86 @@
+// 백준_실버2_BFS_4963번_섬의 개수 문제
+
+
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+
+int map[52][52] = { 0, };
+int visit[52][52] = { 0, };
+
+void init() {
+
+	for (int i = 0; i < 52; i++) {
+		for (int j = 0; j < 52; j++) {
+			map[i][j] = 0;
+			visit[i][j] = 0;
+		}
+	}
+}
+
+void bfs(int h, int w) {
+	queue < pair<int, int>> que;
+	pair<int, int> p;
+	int check_w[8] = {0,1,1,1,0,-1,-1,-1};
+	int check_h[8] = {-1,-1,0,1,1,1,0,-1};
+
+	p.first = h;
+	p.second = w;
+	que.push(p);
+	visit[h][w] = 1;
+
+	while (!que.empty()) {
+		int h1 = que.front().first;
+		int w1 = que.front().second;
+		que.pop();
+
+		for (int k = 0; k < 8; k++) {
+			if (map[h1 + check_h[k]][w1 + check_w[k]] == 1 && visit[h1 + check_h[k]][w1 + check_w[k]] == 0) {
+				p.first = h1 + check_h[k];
+				p.second = w1 + check_w[k];
+				que.push(p);
+				visit[h1 + check_h[k]][w1 + check_w[k]] = 1;
+			}
+		}
+	}
+}
+
+int main() {
+
+	int w, h;
+	int sol = 0;
+
+	while (1) {
+		cin >> w >> h;
+
+		if (w == 0 && h == 0) {
+			break;
+		}
+
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= w; j++) {
+				cin >> map[i][j];
+			}
+		}
+
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= w; j++) {
+				if (map[i][j] == 1 && visit[i][j] == 0) {
+					bfs(i, j);
+					sol++;
+				}
+			}
+		}
+
+		cout << sol << endl;
+
+		sol = 0;
+		init();
+	}
+
+
+
+	return 0;
+}

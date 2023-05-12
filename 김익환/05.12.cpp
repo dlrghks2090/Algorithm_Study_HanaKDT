28f4413be37f75d3c44727c7304415946b9c8beb
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-05-12 11:32:13 +0900

    Add : ATM 문제
    백준_실버4_그리디_11399번_ATM 문제풀이

---
+++ 김익환/05.12.cpp
@@ -0,0 +1,38 @@
+// 백준_실버4_그리디_11399번_ATM 문제
+
+
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	int N;
+	int arr[1001] = { 0, };
+	int num;
+	vector<int> v;
+	int sol = 0;
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			sol += v[j];
+		}
+	}
+
+	cout << sol;
+
+
+	return 0;
+}
